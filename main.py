e0387c9a7ea251c1f1341a2535fd3c7e40a0ad04
Pang Lang <147260128+pang-lang@users.noreply.github.com>
2025-12-18 03:16:55 +0800

    Refactor import statement for load_main function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Main entry point for Radiology VQA Dataset Project
 """
 
-from preprocessing.load_dataset import main as load_main
+from load_dataset import main as load_main
 
 if __name__ == "__main__":
     load_main()
